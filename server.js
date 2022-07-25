504f465813414abd6c9c96c06e7f94ba0b425e15
milfred08 <milfred08@gmail.com>
2022-07-24 21:45:49 -0400

    updating

--- server.js
+++ server.js
@@ -84,5 +84,7 @@ app.use(routes);
 
 // User sequelize to create models if any and then initalize listenting express node.js
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, ()=> console.log('server is running'));
+    app.listen(PORT, () => console.log('Now listening'));
   });
+
+
